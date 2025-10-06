f941a1b421085ae2c70c6ce0a7c8ed9c3695558c
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-06 14:56:35 +1000

    Delete src directory

--- src/abi/erc1155.ts
+++
@@ -1,8 +0,0 @@
-// src/abi/erc1155.ts
-// Minimal ERC-1155 ABI for transfer. Comments in English only.
-export const ERC1155_ABI = [
-  { type: "function", name: "safeTransferFrom", stateMutability: "nonpayable", inputs: [
-      { name: "from", type: "address" }, { name: "to", type: "address" },
-      { name: "id", type: "uint256" }, { name: "value", type: "uint256" }, { name: "data", type: "bytes" }
-    ], outputs: [] },
-] as const;
--- src/abi/erc721.ts
+++
@@ -1,8 +0,0 @@
-// src/abi/erc721.ts
-// Minimal ERC-721 ABI for transfer. Comments in English only.
-export const ERC721_ABI = [
-  { type: "function", name: "ownerOf", stateMutability: "view", inputs: [{ name: "tokenId", type: "uint256" }], outputs: [{ type: "address" }] },
-  { type: "function", name: "safeTransferFrom", stateMutability: "nonpayable", inputs: [
-      { name: "from", type: "address" }, { name: "to", type: "address" }, { name: "tokenId", type: "uint256" }
-    ], outputs: [] },
-] as const;
