cb7531b7fc885d70fd911373eba1c514d8463a6e
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-08 12:48:29 +1000

    Update AudioManager.ts

--- src/audio/AudioManager.ts
+++ src/audio/AudioManager.ts
@@ -30,7 +30,7 @@ class AudioManager {
       return;
     }
     console.info("[Audio] init(): creating audio elements");
-    this.bgmMain = makeAudio("/audio/bgm_main.mp3", true, 0.5);
+    this.bgmMain = makeAudio("/audio/bgm_main.mp3", true, 0.3);
     this.bgmDisaster = makeAudio("/audio/bgm_disaster.mp3", true, 0.25);
     this.sfxEat = makeAudio("/audio/sfx_eat.mp3", false, 0.3);
 
