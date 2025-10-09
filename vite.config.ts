92004281af5f35dee5ad2742aef157436f4c84b5
naive <36657473+iamnaive@users.noreply.github.com>
2025-10-09 21:45:34 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,19 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          wagmi: ["wagmi"],
+          viem: ["viem"],
+          query: ["@tanstack/react-query"],
+        },
+      },
+    },
+  },
 });
